7e45c4ddb34e4d072e39c486ac226a42be4779e4
Heider Lopes <heidertreinamentos@gmail.com>
2017-09-30 17:28:05 -0300

    Acao para abrir a tela de pedido

---
+++ app/src/main/java/br/com/fiap/pizzaria/Constants.java
@@ -0,0 +1,10 @@
+package br.com.fiap.pizzaria;
+
+/**
+ * Created by logonpf on 30/09/2017.
+ */
+
+public class Constants {
+
+    public static final String KEY_LOGIN = "KEY_LOGIN";
+}
---
+++ app/src/main/java/br/com/fiap/pizzaria/MenuActivity.java
@@ -0,0 +1,39 @@
+package br.com.fiap.pizzaria;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MenuActivity extends AppCompatActivity {
+
+    private TextView tvSaudacao;
+    private Button btFazerPedido;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        tvSaudacao = (TextView) findViewById(R.id.tvSaudacao);
+
+        btFazerPedido = (Button) findViewById(R.id.btFazerPedido);
+
+        btFazerPedido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent telaPedido = new Intent(MenuActivity.this,
+                        PedidoActivity.class);
+                startActivity(telaPedido);
+            }
+        });
+
+        if(getIntent() != null) {
+            tvSaudacao.setText(
+                    getString(R.string.hello,
+                    getIntent().getStringExtra(Constants.KEY_LOGIN)));
+        }
+    }
+}
